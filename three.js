b4bc2d3508e1f9cc35785cccb9e7a107fde3fcf0
Kartikey Singh Gahlot <decten.kartz@gmail.com>
2025-04-29 09:56:01 +0530

    Update three.js

--- three.js
+++ three.js
@@ -45,4 +45,49 @@ setTimeout(()=>{
 }
 
 
-control.addEventListener("click" , flip)
+control.addEventListener("click" , flip);
+
+
+
+// ----------------< HEADER NAVIGATION >-----
+
+const mobileContainer = document.querySelector(".mobileContainer");
+const lines = document.querySelectorAll(".lines");
+const nav = document.querySelector("nav");
+const ul = document.querySelector("ul");
+
+let stat = "off";
+
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth > 780) {
+      ul.style.display="flex";
+  }
+  else{
+    ul.style.display="none";
+  }
+});
+
+
+mobileContainer.addEventListener("click",()=>{
+  if(stat != "on"){
+    lines[0].style.transform = "rotateZ(40deg) translateY(0px) translateX(3px)";
+    lines[1].style.display = "none";
+    lines[2].style.transform = "rotateZ(-40deg) translateY(-2px) translateX(4px)";
+    stat = "on";
+    nav.style.height = "100vh";
+    nav.style.width = "100%";
+    nav.style.backgroundColor = "black";
+    ul.style.display = "flex";
+  }
+  else{
+    lines[0].style.transform = "none";
+    lines[1].style.display = "block";
+    lines[2].style.transform = "none";
+    stat = "off";
+    nav.style.height = "fit-content";
+    nav.style.width = "fit-content";
+    nav.style.backgroundColor = "transparent";
+    ul.style.display = "none";
+  }
+});
